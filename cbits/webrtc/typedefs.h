4bb0a89171c016e924cf96692d806be146f8817e
Fan Lan <lanfanllrr@gmail.com>
2023-08-15 10:22:33 +0800

    support riscv64

--- cbits/webrtc/typedefs.h
+++ cbits/webrtc/typedefs.h
@@ -26,6 +26,13 @@
 #elif defined(__aarch64__)
 #define WEBRTC_ARCH_64_BITS
 #define WEBRTC_ARCH_LITTLE_ENDIAN
+#elif __riscv == 1
+#if __riscv_xlen == 64
+#define WEBRTC_ARCH_64_BITS
+#else
+#define WEBRTC_ARCH_32_BITS
+#endif
+#define WEBRTC_ARCH_LITTLE_ENDIAN
 #elif defined(_M_IX86) || defined(__i386__)
 #define WEBRTC_ARCH_X86_FAMILY
 #define WEBRTC_ARCH_X86
